8edbb62227971f4be4ae6eeb0bf005cbb989418c
ELENA ZHAN <elenzhandeva@gmail.com>
2016-09-18 06:13:35 -0400

    sync up 5

--- dist/js/site.js
+++ dist/js/site.js
@@ -37,7 +37,8 @@ site.config(function($stateProvider, $urlRouterProvider) {
     })
     .state('countdown', {
     	url: '/countdown',
-    	templateUrl: 'views/countdown.html'
+    	templateUrl: 'views/countdown.html',
+    	controller: 'CountdownController'
     });
 
 });
@@ -204,6 +205,55 @@ site.controller("MatchController", ["$scope", "$firebaseAuth", "$firebaseArray",
 			console.log("done");
 			console.log(data[0]);
 		});
+	}
+]);
+
+site.controller("CountdownController", ["$scope", "$state",
+	function($scope, $state){
+
+		function getTimeRemaining(endtime) {
+			  var t = Date.parse(endtime) - Date.parse(new Date());
+			  var seconds = Math.floor((t / 1000) % 60);
+			  var minutes = Math.floor((t / 1000 / 60) % 60);
+			  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+			  return {
+			    'total': t,
+			    'days': days,
+			    'hours': hours,
+			    'minutes': minutes,
+			    'seconds': seconds
+			  };
+			}
+
+			function initializeClock(id, endtime) {
+				var clock = document.getElementById(id);
+				// var daysSpan = clock.querySelector('.days');
+				// var hoursSpan = clock.querySelector('.hours');
+				var minutesSpan = clock.querySelector('.minutes');
+				var secondsSpan = clock.querySelector('.seconds');
+
+				function updateClock() {
+					var t = getTimeRemaining(endtime);
+
+					// daysSpan.innerHTML = t.days;
+					// hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+					minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+					secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+					if (t.total <= 0) {
+						clearInterval(timeinterval);
+					}
+				}
+
+				updateClock();
+				var timeinterval = setInterval(updateClock, 1000);
+			}
+
+			var deadline = new Date(Date.parse(new Date()) + 60 * 60 * 1000);
+			initializeClock('clockdiv', deadline);
+
+
 	}
 ]);
 
